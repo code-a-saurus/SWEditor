a4b604486bd3957fb321ee84c09b0e72b5971372
lee <lee@t-rex.bigdinosaur.lan>
2024-12-21 10:50:51 -0600

    Typo

--- src/inventory_constants.py
+++ src/inventory_constants.py
@@ -188,4 +188,4 @@ VALID_INVENTORY = {
     NANOBOT,           # 0x21
     MARK_V_TENG,       # 0x22
     VADROXON,          # 0x23
-}.union(VALID_AMMO)  # Can also store weapons/armor/ammo in inventory
+}.union(VALID_AMMO)  # Can also store ammo in inventory
