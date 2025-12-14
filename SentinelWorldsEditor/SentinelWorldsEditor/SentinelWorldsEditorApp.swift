278b615eff408d3d1cb1fbd957e8ba6843c7a610
lee <lee@t-rex.bigdinosaur.lan>
2025-12-14 07:23:12 -0600

    First commit to swift-native branch

---
+++ SentinelWorldsEditor/SentinelWorldsEditor/SentinelWorldsEditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SentinelWorldsEditorApp.swift
+//  SentinelWorldsEditor
+//
+//  Created by lee on 12/14/25.
+//
+
+import SwiftUI
+
+@main
+struct SentinelWorldsEditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
